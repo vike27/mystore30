19d5042853d4245cf1864ccb668b5f8aaa3bebe8
Javier Noris <norisjavier@gmail.com>
2014-06-16 03:21:19 -0700

    first commit/added basic spree storefront

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mystore30::Application.config.secret_key_base = '6f0fa1d5fcb46d56876b58d49db08224b1cf95c13aeff6f6e6e107df9b3da23d2c56528b71d591e68e374aba6179f3497335145821a35f77fdb6ac1f8c5941f6'
